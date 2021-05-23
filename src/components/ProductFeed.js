142e46d3730eb4ff74f3da2fba257ad7e6e56c31
Siddhesh Kankekar <kankekas@tcd.ie>
2021-05-23 22:05:38 +0100

    Added more Product

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -2,18 +2,59 @@ import Products from "./Products";
 
 function ProductFeed({ products }) {
   return (
-    <div>
-      {products.map(({ id, title, description, price, category, image }) => (
-        <Products
-          key={id}
-          id={id}
-          title={title}
-          description={description}
-          price={price}
-          category={category}
-          image={image}
-        />
-      ))}
+    <div
+      className="grid grid-flow-row-dense md:mt-52 mx-auto
+    md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+    >
+      {products
+        .slice(0, 4)
+        .map(({ id, title, description, price, category, image }) => (
+          <Products
+            key={id}
+            id={id}
+            title={title}
+            description={description}
+            price={price}
+            category={category}
+            image={image}
+          />
+        ))}
+
+      <img
+        className="md:col-span-full"
+        src="https://links.papareact.com/dyz"
+        alt=""
+      />
+
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, description, price, category, image }) => (
+            <Products
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              price={price}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, description, price, category, image }) => (
+          <Products
+            key={id}
+            id={id}
+            title={title}
+            description={description}
+            price={price}
+            category={category}
+            image={image}
+          />
+        ))}
     </div>
   );
 }
